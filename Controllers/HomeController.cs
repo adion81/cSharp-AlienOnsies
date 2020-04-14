2c915c37469990109f39a7650515982e6f4eb831
Adrien Dion <adion81@gmail.com>
2020-04-14 16:00:54 -0500

    Demo code

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using AlienOnsies.Models;
+
+namespace AlienOnsies.Controllers
+{
+    public class HomeController : Controller
+    {
+        static List<Onsie> Stock = new List<Onsie>()
+            {
+                new Onsie()
+                {
+                    Fabric = "100% Genuine Polyester",
+                    Alien = "Xenomorph",
+                    Color ="Grey",
+                    ImgUrl = "https://www.4kigurumi.com/image/cache/data/kigurumi/D011/alien-kigurumi-onesie-5-600x900.jpg",
+                    isFooted = false,
+                    Price = 59.99M
+                },
+                new Onsie()
+                {
+                    Fabric = "Satin",
+                    Alien = "Totoro",
+                    Color ="Greige",
+                    ImgUrl = "https://www.onesieshow.com/media/catalog/product/cache/1/image/448x597/9df78eab33525d08d6e5fb8d27136e95/y/a/yaw712026.jpg",
+                    isFooted = false,
+                    Price = 19.99M
+                }
+            };
+
+        [HttpGet("")]
+        public IActionResult Index()
+        {
+            string welcome = "This is a site dedicated to delivering the best alien onsies";
+
+            return View("Index",welcome);
+        }
+
+        [HttpGet("onsies")]
+        public IActionResult Onsies()
+        {
+            return View(Stock);
+        }
+
+        [HttpPost("create")]
+        public IActionResult CreateOnsie(Onsie newOnsie)
+        {
+            Stock.Add(newOnsie);
+            return Redirect("/onsies");
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Models/Onsie.cs
@@ -0,0 +1,12 @@
+namespace AlienOnsies.Models
+{
+    public class Onsie
+    {
+        public string Fabric { get; set; }
+        public string Alien { get; set; }
+        public string ImgUrl { get; set; }
+        public string Color { get; set; }
+        public bool isFooted { get; set; }
+        public decimal Price { get; set; }
+    }
+}
